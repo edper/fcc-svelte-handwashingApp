87054a0c8f1814470ae15f37587c709346298eea
edper <edpermail@yahoo.com>
2020-05-12 00:30:35 +1100

    Create Timer

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -1,5 +1,23 @@
 <script>
     import ProgressBar from './ProgressBar.svelte';
+
+    const totalSeconds = 5;
+    let secondsLeft = totalSeconds;
+    let isRunning = false;
+
+    $: progress= ((totalSeconds-secondsLeft)/totalSeconds)*100;
+
+    function handleClick() {        
+        isRunning = true;
+        const id = setInterval(() => {
+            secondsLeft--;
+            if (secondsLeft===0) {
+                clearInterval(id);
+                isRunning=false;
+                progress=0;
+            }
+        }, 1000);
+    }
 </script>
 <style>
     h2 {
@@ -11,12 +29,23 @@
         width:100%;
         margin:10px 0;
     }
+    .start[disabled] {
+        background-color:rgb(194,194,194);
+        cursor:not-allowed;
+    }
+
 </style>
 
 <div bp="grid">
-    <h2 bp="offset-5@md 4@md 12@sm">Seconds Left</h2>
+    <h2 bp="offset-5@md 4@md 12@sm">
+        Seconds Left: {secondsLeft}
+    </h2>
 </div>
-<ProgressBar/>
-<button class="start">
-    Start
-</button>
+<ProgressBar {progress}/>
+<div bp="grid">
+    <button disabled={isRunning} bp="offset-5@md 4@md 12@sm" class="start"
+     on:click={handleClick}
+    >
+        Start
+    </button>
+</div>
