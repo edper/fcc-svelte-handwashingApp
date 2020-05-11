5c45d47f370dc6f1bcb6d5bc02254797bc9ee5a1
edper <edpermail@yahoo.com>
2020-05-11 22:46:57 +1100

    Committing HTML

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,23 @@
-<h1>Hello Svelte</h1>
+<script>
+    import Timer from './Timer.svelte';
+    import HowTo from './HowTo.svelte';
+
+</script>
+<style>
+    h1, h3 {
+        text-align: center;
+    }
+</style>
+
+<h1>Handwashing App</h1>
+<Timer/>
+<HowTo/>
+
+<h3>
+    <a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+        Picture Source
+    </a>
+    <a href="https://www.who.int/gpsc/clean_hands_protection/en/">
+        Sound Source
+    </a>
+</h3>
